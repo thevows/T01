42593e2b19e98e1ed7635535bd1b4786bf0620bc
thevows <665389@qq.com>
2018-02-04 20:44:28 +0800

    test

--- app/src/main/java/sms/reply/constant/TaskCode.java
+++
@@ -1,7 +0,0 @@
-package sms.reply.constant;
-
-public class TaskCode {
-    public static final int TASK_FETCH_SMS=1;
-    public static final int TASK_START_OBSERVER=2;
-    public static final int TASK_STOP_OBSERVER=3;
-}
--- app/src/main/java/sms/reply/constant/TaskStatus.java
+++
@@ -1,5 +0,0 @@
-package sms.reply.constant;
-
-public class TaskStatus {
-	public static final int TASK_FINISHED=1;
-}
